e7b8654a492ba3317e08e0ce8666875e534dd0ad
Ashish <127130546+akrathor18@users.noreply.github.com>
2026-05-15 16:09:43 +0530

    nndd

--- app/routes/webhooks.app.uninstalled.jsx
+++ app/routes/webhooks.app.uninstalled.jsx
@@ -1,34 +1,34 @@
 
 
-import { authenticate, sessionStorage } from "../shopify.server";
-
-export const action = async ({ request }) => {
-  const { shop, session, topic } = await authenticate.webhook(request);
-
-  console.log(`Received ${topic} webhook for ${shop}`);
-
-  if (session) {
-    // Delete all sessions for this shop from MongoDB
-    await sessionStorage.deleteSessions(shop);
-  }
-
-  return new Response();
-};
-
-
-// import { authenticate } from "../shopify.server";
-// import db from "../db.server";
+// import { authenticate, sessionStorage } from "../shopify.server";
 
 // export const action = async ({ request }) => {
 //   const { shop, session, topic } = await authenticate.webhook(request);
 
 //   console.log(`Received ${topic} webhook for ${shop}`);
 
-//   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
-//   // If this webhook already ran, the session may have been deleted previously.
 //   if (session) {
-//     await db.session.deleteMany({ where: { shop } });
+//     // Delete all sessions for this shop from MongoDB
+//     await sessionStorage.deleteSessions(shop);
 //   }
 
 //   return new Response();
 // };
+
+
+import { authenticate } from "../shopify.server";
+import db from "../db.server";
+
+export const action = async ({ request }) => {
+  const { shop, session, topic } = await authenticate.webhook(request);
+
+  console.log(`Received ${topic} webhook for ${shop}`);
+
+  // Webhook requests can trigger multiple times and after an app has already been uninstalled.
+  // If this webhook already ran, the session may have been deleted previously.
+  if (session) {
+    await db.session.deleteMany({ where: { shop } });
+  }
+
+  return new Response();
+};
